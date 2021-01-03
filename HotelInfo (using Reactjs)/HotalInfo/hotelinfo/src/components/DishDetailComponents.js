533f59089a10e111b22702a0335cecc245394f05
Sunil <sunilkpr2k1@gmail.com>
2021-01-03 17:18:53 +0530

    Fetch from Server

--- HotelInfo (using Reactjs)/HotalInfo/hotelinfo/src/components/DishDetailComponents.js
+++ HotelInfo (using Reactjs)/HotalInfo/hotelinfo/src/components/DishDetailComponents.js
@@ -18,6 +18,7 @@ import {
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -130,7 +131,7 @@ function RenderDish({dish}) {
     return(
         <div>
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
