19914f9b1cca5577eb2dc89288db7abb1cd04083
Sunil <sunilkpr2k1@gmail.com>
2021-01-08 14:29:13 +0530

    MyPortfolio

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import MyNavbar  from "./components/my-navbar/my-navbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about/about.component";
+import { Parallax } from "react-parallax";
+import Fade from "react-reveal/Fade";
+import Container from "react-bootstrap/Container";
+import Skills from "./pages/skills/skills.component";
+import Slide from "react-reveal/Slide";
+import Experience from "./pages/experience/experience.component";
+import Particles from "react-particles-js";
+import { particlesOptions } from "./particlesOptions";
+import TimeLine from "./components/projects-timeline/projects-timeline.component";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyNavbar />
+      <MyCarousal />
+      <TitleMessage />
+      <Particles
+        className="particles particles-box"
+        params={particlesOptions}
+      /> 
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <hr />
+
+            <Experience />
+          </Fade>
+        </Container>
+      </div>
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <TimeLine />
+        </Slide>
+      </Container>
+    </div>
+    
+  );
+}
+
+export default App;
