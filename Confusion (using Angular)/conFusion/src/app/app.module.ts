0ff1e209d9f18012bd0d6b189dc173c9cd33e0e5
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-07-02 16:42:43 +0530

    components part 1 completed

--- Confusion (using Angular)/conFusion/src/app/app.module.ts
+++ Confusion (using Angular)/conFusion/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-
+import { MatListModule } from '@angular/material';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Confusion (using Angular)/conFusion/src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+
+}
