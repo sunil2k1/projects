cd9df146c13412f4b32356c4a5d805620be0d11f
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-07-04 16:27:03 +0530

    Data Binding

--- Confusion (using Angular)/conFusion/src/app/shared/dish.ts
+++ Confusion (using Angular)/conFusion/src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './comment';
 export class Dish {
     id: string;
     name: string;
@@ -7,5 +8,5 @@ export class Dish {
     label: string;
     price: string;
     description: string;
-
+    comments:Comment[];
 }
