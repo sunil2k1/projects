9c143a47000b5e5b9b614c176cebd20964121d75
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-07-06 12:55:40 +0530

    angular router

--- Confusion (using Angular)/conFusion/src/app/app.module.ts
+++ Confusion (using Angular)/conFusion/src/app/app.module.ts
@@ -12,9 +12,14 @@ import { MenuComponent } from './menu/menu.component';
 import { MatCardModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +27,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +40,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
