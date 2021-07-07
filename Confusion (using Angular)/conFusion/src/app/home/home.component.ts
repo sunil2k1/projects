17639f01e74892c21b7d2bcb1422916abdc84f2a
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-07-07 12:15:12 +0530

    Assingment 2

--- Confusion (using Angular)/conFusion/src/app/home/home.component.ts
+++ Confusion (using Angular)/conFusion/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +14,16 @@ import { PromotionService } from '../services/promotion.service';
 export class HomeComponent implements OnInit {
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
+
   constructor(private dishService: DishService,
-    private promotionService: PromotionService) { }
+    private promotionService: PromotionService,
+    private leaderService: LeaderService ) { }
 
   ngOnInit() {
     this.dish = this.dishService.getFeaturedDish();
-    this.promotion=this.promotionService.getFeaturedPromotion();
+    this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 
 }
