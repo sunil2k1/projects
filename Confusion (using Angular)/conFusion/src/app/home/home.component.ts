a0327ed6e4b75751baffc54344fa323439dff77f
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-07-09 18:02:07 +0530

    Promise Part 1

--- Confusion (using Angular)/conFusion/src/app/home/home.component.ts
+++ Confusion (using Angular)/conFusion/src/app/home/home.component.ts
@@ -21,9 +21,12 @@ export class HomeComponent implements OnInit {
     private leaderService: LeaderService ) { }
 
   ngOnInit() {
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
-    this.leader = this.leaderService.getFeaturedLeader();
+    this.dishService.getFeaturedDish()
+    .then(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion()
+    .then(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+    .then(leader => this.leader = leader);
   }
 
 }
